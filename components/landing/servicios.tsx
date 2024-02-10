1ba4a75b2ffbbd20a6d6afd399221730cc98364c
alguadmar <alguadmar@gmail.com>
2024-02-10 16:36:05 -0600

    fixed key bug

--- components/landing/servicios.tsx
+++ components/landing/servicios.tsx
@@ -7,8 +7,8 @@ const Servicios = ({ id }: { id: string }) => {
     <section id={id} className="mt-36 sm:mt-0">
       <p className="text-center font-semibold text-3xl mb-20">Servicios</p>
       {services.map((service) => (
-        <Accordion variant="bordered" className="w-[75%] m-auto mt-5">
-            <AccordionItem id={service.title} aria-label={service.title} title={service.title}>
+        <Accordion key={service.title} variant="bordered" className="w-[75%] m-auto mt-5">
+            <AccordionItem aria-label={service.title} title={service.title}>
                 <p>{service.description}</p>
             </AccordionItem>
         </Accordion>
